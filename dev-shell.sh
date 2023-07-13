5c2940a9224f77eef84e7ee43af5ccff42cbd0d7
Neosapien <anas@lecaillon.com>
2023-07-13 23:08:36 +0200

    fixed non-functionning script

--- dev-shell.sh
+++ dev-shell.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
-programs=$(cat .dev \ # read list of programs from ".dev" file
-  | sed 's/#.*$//g' \ # remove comments
-  | tr '\n' ' ' \ # change newlines to spaces
-  | xargs echo) # remove excess spaces
+programs=$(sed 's/#.*$//g' .dev | # remove comments
+  tr '\n' ' ' | # change newlines to spaces
+  xargs echo) # remove excess spaces
 
 nix-shell -p $programs
